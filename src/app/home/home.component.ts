b62553b450615a0a4a71160e1ae63555175536a8
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-03-16 16:57:34 +0700

    Separate home component into each component smaller than before

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 // import { ToastrService } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
-
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
