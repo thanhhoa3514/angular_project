de2b279b51917e158a297a55c123645a5552b508
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-03-16 22:58:18 +0700

    Enhance layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RegisterComponent],
-  template: '<app-register></app-register>'
+  imports: [HomeComponent],
+  template: '<app-home></app-home>'
 })
 export class AppComponent {
   title = 'shopapp-angular';
