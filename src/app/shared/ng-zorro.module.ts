cacd0a0731143948c51d85a40fbb52d4c333f959
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-04 09:12:45 +0700

    fix : Update ng-zoro module for reduce the load

--- src/app/shared/ng-zorro.module.ts
+++ src/app/shared/ng-zorro.module.ts
@@ -38,10 +38,10 @@ import { IconDefinition } from '@ant-design/icons-angular';
 import * as AllIcons from '@ant-design/icons-angular/icons';
 
 // Tạo mảng chứa tất cả các icon
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+  // const antDesignIcons = AllIcons as {
+  //   [key: string]: IconDefinition;
+  // };
+//const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 const NzModules = [
   NzButtonModule,
